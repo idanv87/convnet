293b880c56e64a57cbd751da0b918c6354b565f2
idanv87 <idanv@campus.technion.ac.il>
2024-08-05 17:46:58 +0300

    first

--- main.py
+++ main.py
@@ -109,7 +109,7 @@ def generate_data(names,  save_path, number_samples,Seed):
 if __name__=='__main__':
     pass
 # if False: 400 is good for n=20
-    generate_data(names, Constants.train_path, number_samples=10, Seed=1)
+    generate_data(names, Constants.train_path, number_samples=300, Seed=1)
     generate_data(test_names,Constants.test_path,number_samples=1, Seed=800)
 
 else:
