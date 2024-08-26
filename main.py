0b777189f0d5c8fe928c51f9641b2ae248bca238
idanv87 <idanv@campus.technion.ac.il>
2024-08-27 02:41:40 +0300

    first

--- main.py
+++ main.py
@@ -33,7 +33,10 @@ names=[np.array([[0,0],[1,0],[1,1],[0,1],[0,0]]),
        np.array([[0,0],[1,0],[1,1],[0.5,1],[0.5,0.5],[0,0.5],[0,0]]),
        np.array([[0,0],[0.5,0],[0.5,0.5],[1,0.5],[1,1],[0,1],[0,0]])
        ]
-test_names=np.array([[0,0],[1,0],[1,5/14],[9/14,5/14],[9/14,1],[4/14,1],[4/14,5/14],[0,5/14],[0,0]])
+hight=1
+test_names=[np.array([[0,0],[1,0],[1,0.5],[9/14,0.5],[9/14,hight],[7/14,hight],
+                       [7/14,0.5],[5/14,0.5],[5/14,hight],[3/14,hight],[3/14,0.5],
+                       [0,0.5],[0,0]])]
 # names=[names[0]]
 # names=[]
 # for k in range(1,4):
@@ -119,7 +122,7 @@ def generate_data(names,  save_path, number_samples,Seed):
 if __name__=='__main__':
     pass
 # if False: 400 is good for n=20
-    generate_data(names, Constants.train_path, number_samples=300, Seed=1)
+    # generate_data(names, Constants.train_path, number_samples=300, Seed=1)
     generate_data(test_names,Constants.test_path,number_samples=1, Seed=800)
 
 else:
